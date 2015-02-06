a8689656d0cce09e49780934669a1e7ee0fba23a
Jean Lucas de Carvalho <jeanlucasdecarvalho@gmail.com>
2015-02-05 23:02:07 -0200

    Tirinhas 1.0

--- www/js/config.js
+++ www/js/config.js
@@ -1,27 +1,12 @@
 var feeds = [
-    { name: 'Experiências Empreendedoras', url: 'https://medium.com/feed/minhas-experiencias'},
-    { name: 'Brasil Empreendedor', url: 'https://medium.com/feed/brasil-empreendedor'},
-    { name: 'Empreendedorismo: Primeiros Passos' , url: 'https://medium.com/feed/empreendedorismo-primeiros-passos'},
-    { name: 'Histórias de Empreendedorismo', url: 'https://medium.com/feed/historias-de-empreendedorismo'},
-    { name: 'AppTicket Team', url: 'https://medium.com/feed/appticket-team'},
-    { name: 'Info Money - Startup', url: 'http://www.infomoney.com.br/negocios/startups/rss'},
-    { name: 'Catraca Livre - Negócios', url: 'https://catracalivre.com.br/brasil/editoria/negocio-urbanidade/feed/'},
-    { name: 'Startupi', url: 'http://startupi.com.br/feed/'},
-    { name: 'Endeavor', url: 'https://endeavor.org.br/?feed=/feed/endeavor-portal'},
-    { name: 'Bizstart', url: 'http://bizstart.com.br/feed/'},
-    { name: 'Administradores', url: 'http://www.administradores.com.br/rss/artigos/'},
-    { name: 'Pensando Grande', url: 'http://www.pensandogrande.com.br/feed/'},
-    { name: 'Empreendedores Criativos', url: 'http://www.empreendedorescriativos.com.br/feed/'},
-    { name: 'Empreendedor Digital', url: 'http://www.empreendedor-digital.com/feed'},
-    { name: 'Blog da Anjos do Brasil', url: 'http://blog.anjosdobrasil.net/feeds/posts/default'},
-    { name: 'Jornal do Empreendedor', url: 'http://feeds.feedburner.com/jornalempreendedor'},
-    { name: 'Saia do Lugar', url: 'http://feeds.feedburner.com/SaiaDoLugar'},
-    { name: 'Guia da Startup', url: 'http://www.guiadastartup.com.br/feed/'},
-    { name: 'Startup SC', url: 'http://www.startupsc.com.br/feed/'},
-    { name: 'Marcelo Toledo', url: 'http://feeds.feedburner.com/marcelotoledo'},
-    { name: 'Startupenado', url: 'http://feeds.feedburner.com/com/aqsv'},
-    { name: 'Blog Geração de Valor', url: 'http://geracaodevalor.com/blog/feed/'},
-    { name: 'Estadão - Blog do Empreendedor', url: 'http://blogs.pme.estadao.com.br/blog-do-empreendedor/feed/'}
+    { name: 'Dr. Pepper', url: 'http://feeds.feedburner.com/drpepper'},
+    { name: 'Um Sábado Qualquer', url: 'http://feeds.feedburner.com/umsabadoqualquer/olOP'},
+    { name: 'Will Tirando' , url: 'http://www.willtirando.com.br/rss/'},
+    { name: 'Mentirinhas', url: 'http://feeds.feedburner.com/mentirinhas'},
+    { name: 'Gi & Kim', url: 'http://www.giekim.com/feeds/posts/default?alt=rss'},
+    { name: 'Vida de Suporte', url: 'http://feeds.feedburner.com/vidasuporte'},
+    { name: 'Digo Freitas', url: 'http://digofreitas.com/feed/'},
+    { name: 'Depósito de Tirinhas', url: 'http://deposito-de-tirinhas.tumblr.com/rss'}
 ];
 
 var admobid = {};
@@ -42,4 +27,4 @@ if( /(android)/i.test(navigator.userAgent) ) { // for android
     };
 }
 
-var colorClass = 'balanced';
+var colorClass = 'royal';
